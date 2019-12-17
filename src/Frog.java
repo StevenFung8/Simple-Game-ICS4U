c9dc8a87206c15c1c3bd84f1b6a823cfbeadb748
Steven Fung <32400622@student.publicboard.ca>
2019-12-17 14:17:53 -0500

    yessir new stuff GIT WORKS

---
+++ src/Frog.java
@@ -0,0 +1,9 @@
+/*class Frog {
+    private int posX,posY;
+    public Frog(){
+        posX = 100;
+        posY = 100;
+    }
+    public void move()
+}
+*/
--- src/Frogger.java
+++ src/Frogger.java
@@ -1,6 +1,6 @@
+import javax.swing.*;
 public class Frogger {
     public static void main (String [] args){
-        System.out.println("poop");
         System.out.println("vagina");
     }
 }
