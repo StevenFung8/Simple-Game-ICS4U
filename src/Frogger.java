eab30581ea9c611dd837645e12c77848a4c7f77e
dtan3 <dtan3771@gmail.com>
2020-01-10 13:50:27 -0500

    You're gay

--- src/Frogger.java
+++ src/Frogger.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
 
 public class Frogger extends JFrame{
     Timer myTimer;
@@ -12,7 +15,7 @@ public class Frogger extends JFrame{
     public Frogger() {
         super("Frogger Dylan & Steven ltd copyright");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(810,785);
+        setSize(1080,700);
 
         myTimer = new Timer(10, new TickListener());
         myTimer.start();
@@ -27,7 +30,7 @@ public class Frogger extends JFrame{
     class TickListener implements ActionListener{
         public void actionPerformed(ActionEvent evt){
             if(game!= null && game.ready){
-                game.move();
+
                 game.repaint();
             }
         }
@@ -37,62 +40,76 @@ public class Frogger extends JFrame{
         Frogger frame = new Frogger();
     }
 }
+class Obstacle{
+    private int sizex,sizey;
+    private Image pic;
 
+}
 class GamePanel extends JPanel implements KeyListener {
-    private int destx,desty,boxx,boxy;
+    private int frogx,frogy;
     public boolean ready=false;
     private boolean gotName=false;
-    private boolean []keys;
+    private boolean []keysPressed;
     private Image back;
 
     public GamePanel(){
-        try {
-            back = ImageIO.read(new File("Pictures/froggerBackground.png"));
-        }
-        catch (IOException e) {
-        }
-        keys = new boolean[KeyEvent.KEY_LAST+1];
+        keysPressed = new boolean[KeyEvent.KEY_LAST+1];
         back = new ImageIcon("OuterSpace.jpg").getImage();
         frogx=200;
         frogy=200;
         addKeyListener(this);
-        setSize(800,751);
-    }
-    public void keyTyped(KeyEvent e) {}
-
-    public void keyPressed(KeyEvent e) {
-        keys[e.getKeyCode()] = true;
-    }
-
-    public void keyReleased(KeyEvent e) {
-        keys[e.getKeyCode()] = false;
+        setSize(1080,700);
     }
 
 
     public void addNotify() {
+
         super.addNotify();
         requestFocus();
         ready = true;
     }
 
 
+
     @Override
     public void paintComponent(Graphics g){
-        g.setColor(new Color(222,222,255));
+        g.setColor(new Color(255,222,222));
         g.fillRect(0,0,getWidth(),getHeight());
         g.setColor(Color.blue);
         g.fillRect(frogx,frogy,40,40);
     }
 
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
     }
 
     @Override
-    public void keyPressed(KeyEvent keyEvent) {
+    public void keyPressed(KeyEvent k) {
+        System.out.println("skfkabdk");
+        if(k.getKeyCode()==KeyEvent.VK_RIGHT && !keysPressed[KeyEvent.VK_RIGHT]){
+            System.out.println("Right");
+            frogx+=40;
+        }
 
+        if(k.getKeyCode()==KeyEvent.VK_LEFT && !keysPressed[KeyEvent.VK_LEFT]){
+            System.out.println("Left");
+            frogx-=40;
+        }
+        if(k.getKeyCode()==KeyEvent.VK_UP && !keysPressed[KeyEvent.VK_UP]){
+            System.out.println("Up");
+            frogy-=40;
+        }
+        if(k.getKeyCode()==KeyEvent.VK_DOWN && !keysPressed[KeyEvent.VK_DOWN]){
+            System.out.println("Down");
+            frogy+=40;
+        }
+        keysPressed[k.getKeyCode()]=true;
 
+    }
 
     @Override
-    public void keyReleased(KeyEvent keyEvent) {
-
+    public void keyReleased(KeyEvent k) {
+        keysPressed[k.getKeyCode()]=false;
     }
 }
